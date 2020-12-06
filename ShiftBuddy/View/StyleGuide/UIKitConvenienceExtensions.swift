78d7e723bc1854def0662606a26e9a34c13ae8f4
Brandon Fong <50242218+bfongbfong@users.noreply.github.com>
2020-12-06 16:32:04 -0500

    Adds convenience extension to tap gesture to make clicking a substring possible

--- ShiftBuddy/View/StyleGuide/UIKitConvenienceExtensions.swift
+++ ShiftBuddy/View/StyleGuide/UIKitConvenienceExtensions.swift
@@ -16,3 +16,35 @@ extension UIColor {
         self.init(red: CGFloat(red) / 255.0,green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
     }
 }
+
+extension UITapGestureRecognizer {
+
+    /// Allows for a substring to be distinguished within a tap gesture, so a user can tap only a part of a sentence as a button.
+    /// Reference: https://stackoverflow.com/questions/36043006/swift-tap-on-a-part-of-text-of-uilabel
+    func didTapAttributedTextInLabel(label: UILabel, inRange targetRange: NSRange) -> Bool {
+        // Create instances of NSLayoutManager, NSTextContainer and NSTextStorage
+        let layoutManager = NSLayoutManager()
+        let textContainer = NSTextContainer(size: CGSize.zero)
+        let textStorage = NSTextStorage(attributedString: label.attributedText!)
+
+        // Configure layoutManager and textStorage
+        layoutManager.addTextContainer(textContainer)
+        textStorage.addLayoutManager(layoutManager)
+
+        // Configure textContainer
+        textContainer.lineFragmentPadding = 0.0
+        textContainer.lineBreakMode = label.lineBreakMode
+        textContainer.maximumNumberOfLines = label.numberOfLines
+        let labelSize = label.bounds.size
+        textContainer.size = labelSize
+
+        // Find the tapped character location and compare it to the specified range
+        let locationOfTouchInLabel = self.location(in: label)
+        let textBoundingBox = layoutManager.usedRect(for: textContainer)
+        let textContainerOffset = CGPoint(x: (labelSize.width - textBoundingBox.size.width) * 0.5 - textBoundingBox.origin.x, y: (labelSize.height - textBoundingBox.size.height) * 0.5 - textBoundingBox.origin.y)
+
+        let locationOfTouchInTextContainer = CGPoint(x: locationOfTouchInLabel.x - textContainerOffset.x, y: locationOfTouchInLabel.y - textContainerOffset.y)
+        let indexOfCharacter = layoutManager.characterIndex(for: locationOfTouchInTextContainer, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+        return NSLocationInRange(indexOfCharacter, targetRange)
+    }
+}
